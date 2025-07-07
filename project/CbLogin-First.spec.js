e4e0f19b87c5995025453126a92d917ef3de5792
tqphong02 <toquocphong25092002@gmail.com>
2025-07-07 11:31:22 +0700

    update

--- project/CbLogin-First.spec.js
+++ project/CbLogin-First.spec.js
@@ -1,7 +1,15 @@
 import { test } from '@playwright/test';
 import { addAllureMetadata } from '../utils/addAllureMetadata.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 test('@login Login and save session', async ({ page }, testInfo) => {
+    const username = process.env.CB_USERNAME;
+    const password = process.env.CB_PASSWORD;
+
+    if (!username || !password) {
+        throw new Error('Thiếu biến môi trường CB_USERNAME hoặc CB_PASSWORD');
+    }
 
     addAllureMetadata(testInfo, {
         feature: 'Checklist Chất xếp',
@@ -10,13 +18,11 @@ test('@login Login and save session', async ({ page }, testInfo) => {
         severity: 'critical',
         tag: ['web', 'uat'],
     });
+
     await page.goto('https://cabinbook-ui-uat.azurewebsites.net/auth/sign-in');
-    await page.getByRole('textbox', { name: 'Tên tài khoản' }).fill('tentaikhoan');
-    await page.getByRole('textbox', { name: 'Mật khẩu' }).fill('matkhau');
+    await page.getByRole('textbox', { name: 'Tên tài khoản' }).fill(username);
+    await page.getByRole('textbox', { name: 'Mật khẩu' }).fill(password);
     await page.getByRole('button', { name: 'Đăng nhập' }).click();
     await page.waitForTimeout(3000);
-    // await page.pause();
-
-    //Lưu session vào file
     await page.context().storageState({ path: 'auth/storageState.json' });
 });
